dcfc76879e2b52c514d861af6ced3c64707db873
ohmarmirza <omarmirza@gmail.com>
2022-04-19 15:15:24 -0400

    implementing auth lambda

--- backend/src/lambda/auth/auth0Authorizer.ts
+++ backend/src/lambda/auth/auth0Authorizer.ts
@@ -9,10 +9,7 @@ import { JwtPayload } from '../../auth/JwtPayload'
 
 const logger = createLogger('auth')
 
-// TODO: Provide a URL that can be used to download a certificate that can be used
-// to verify JWT token signature.
-// To get this URL you need to go to an Auth0 page -> Show Advanced Settings -> Endpoints -> JSON Web Key Set
-const jwksUrl = '...'
+const jwksUrl = process.env.AUTH_0_JWKS_URL
 
 export const handler = async (
   event: CustomAuthorizerEvent
@@ -58,10 +55,37 @@ async function verifyToken(authHeader: string): Promise<JwtPayload> {
   const token = getToken(authHeader)
   const jwt: Jwt = decode(token, { complete: true }) as Jwt
 
-  // TODO: Implement token verification
-  // You should implement it similarly to how it was implemented for the exercise for the lesson 5
-  // You can read more about how to do this here: https://auth0.com/blog/navigating-rs256-and-jwks/
-  return undefined
+  const response = await Axios.get(jwksUrl)
+  const keys = response.data.keys
+
+  if (!keys || !keys.length) {
+    throw new Error('The JWKS endpoint did not contain any keys');
+  }
+
+  const signingKeys = keys
+    .filter(key => key.use === 'sig' // JWK property `use` determines the JWK is for signature verification
+      && key.kty === 'RSA' // We are only supporting RSA (RS256)
+      && key.kid           // The `kid` must be present to be useful for later
+      && ((key.x5c && key.x5c.length) || (key.n && key.e)) // Has useful public keys
+    ).map(key => {
+      return { kid: key.kid, nbf: key.nbf, publicKey: certToPEM(key.x5c[0]) };
+    });
+
+  // If at least one signing key doesn't exist we have a problem
+  if (!signingKeys.length) {
+    throw new Error('The JWKS endpoint did not contain any signature verification keys');
+  }
+
+  const signingKey = signingKeys.find(key => key.kid === jwt['header']['kid']);
+
+  if (!signingKey) {
+    throw new Error(`Unable to find a signing key that matches '${jwt['header']['kid']}'`)
+  }
+
+  const cert = signingKey.publicKey
+  logger.info('Valid certificate found', cert)
+
+  return verify(token, cert, { algorithms: ['RS256'] }) as JwtPayload
 }
 
 function getToken(authHeader: string): string {
@@ -74,4 +98,10 @@ function getToken(authHeader: string): string {
   const token = split[1]
 
   return token
+}
+
+function certToPEM(cert: string): string {
+  cert = cert.match(/.{1,64}/g).join('\n')
+  cert = `-----BEGIN CERTIFICATE-----\n${cert}\n-----END CERTIFICATE-----\n`
+  return cert
 }
